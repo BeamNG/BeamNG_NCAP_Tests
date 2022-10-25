708cd872b9cb51ca166127d87b4d6b9a95662d97
Mattia Vicari <mattia.vic99@gmail.com>
2022-10-25 18:50:16 +0200

    load ccrm.py file

---
+++ examples/ccrm.py
@@ -0,0 +1,24 @@
+from beamngpy import BeamNGpy
+from beamng_ncap.scenarios import CCRM, generate_scenario
+
+if __name__ == '__main__':
+    beamng = BeamNGpy('localhost', 64256)
+    beamng.open()
+    scenario = generate_scenario('etk800', 'etk800')
+    scenario.make(beamng)
+
+    beamng.set_deterministic()
+    beamng.set_steps_per_second(100)
+    beamng.load_scenario(scenario)
+    beamng.pause()
+    beamng.start_scenario()
+
+    print('Car-to-Car Rear stationary scenario')
+    overlap = 50  # -75, -50, 50, 75 or 100 %
+    vut_speed = 50  # 10, 15, 20, 25, 30, 35, 40, 45 or 50 km/h
+    test = CCRM(beamng, vut_speed, overlap=100)
+    # Add custom sensors here (electrics, damage & timer are already attached)
+    # test.vut.attach_sensor(...)
+    sensors = test.load()
+
+    input('Press enter to continue...')
