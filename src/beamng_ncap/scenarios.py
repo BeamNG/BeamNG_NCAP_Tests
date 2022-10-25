7b61925812cab8d4a877551fb0c50e63c6d084da
Mattia Vicari <mattia.vic99@gmail.com>
2022-10-25 19:45:31 +0200

    upgrade scenarios.py

--- src/beamng_ncap/scenarios.py
+++ src/beamng_ncap/scenarios.py
@@ -2,7 +2,6 @@
 Scenarios for testing autonomous emergency braking (AEB), emergency lane
 keeping (ELK) and lane keep assist (LKA) systems based on European New Car
 Assessment Programme (Euro NCAP) test protocols.
-
 References:
 [1] European New Car Assessment Programme. Test Protocol - AEB Car-to-Car
     systems. Version 3.0.2. July 2019.
@@ -10,96 +9,91 @@ References:
 [2] European New Car Assessment Programme. Test Protocol - AEB VRU systems.
     Version 3.0.2. July 2019.
     https://cdn.euroncap.com/media/53153/euro-ncap-aeb-vru-test-protocol-v302.pdf
-
 .. moduleauthor:: Sedonas <https://github.com/Sedonas>
 .. moduleauthor:: Marc Müller <mmueller@beamng.gmbh>
 """
 
 from abc import ABC, abstractmethod
+from typing import Dict, List, Tuple
 
 import numpy as np
-
-from beamngpy import Road, Scenario, ScenarioObject, Vehicle
+from beamngpy import BeamNGpy, Road, Scenario, ScenarioObject, Vehicle
 from beamngpy.sensors import Damage, Electrics, Timer
 
+Pos = Tuple[float, float, float]
+Quat = Tuple[float, float, float, float]
 
-def generate_scenario(vut_model, gvt_model):
+
+def generate_scenario(vut_model: str, gvt_model: str) -> Scenario:
     """
     Generates the Scenario.
-
     Args:
         vut_model (str): Model of the VUT.
         gvt_model (str): Model of the GVT.
     """
     scenario = Scenario('smallgrid', 'ncap_scenario')
     road1 = Road('track_editor_C_center', rid='ncap_road1')
-    road1.nodes = [(0, 5000, 0, 7), (0, -5000, 0, 7)]
+    road1.add_nodes((0, 5000, 0, 7), (0, -5000, 0, 7))
     scenario.add_road(road1)
 
     road2 = Road('track_editor_C_center', rid='ncap_road2', interpolate=False)
-    road2.nodes = [(500, -1000, 0, 7), (500, 1000, 0, 7)]
+    road2.add_nodes((500, -1000, 0, 7), (500, 1000, 0, 7))
     scenario.add_road(road2)
 
     road3 = Road('track_editor_C_center', rid='ncap_road3', interpolate=False)
-    road3.nodes = [(250, 0, 0, 7), (750, 0, 0, 7)]
+    road3.add_nodes((250, 0, 0, 7), (750, 0, 0, 7))
     scenario.add_road(road3)
 
-    curve1 = Road('track_editor_C_center',
-                  rid='ncap_curve1', interpolate=False)
-    curve1.nodes = _generate_curve(500, -11.5, 270, 178, -2, 11.5, 7)
+    curve1 = Road('track_editor_C_center', rid='ncap_curve1', interpolate=False)
+    curve1.add_nodes(*_generate_curve(500, -11.5, 270, 178, -2, 11.5, 7))
     scenario.add_road(curve1)
 
-    curve2 = Road('track_editor_C_center',
-                  rid='ncap_curve2', interpolate=False)
-    curve2.nodes = _generate_curve(500, -11.5, 90, 182, 2, 11.5, 7)
+    curve2 = Road('track_editor_C_center', rid='ncap_curve2', interpolate=False)
+    curve2.add_nodes(*_generate_curve(500, -11.5, 90, 182, 2, 11.5, 7))
     scenario.add_road(curve2)
 
-    curve3 = Road('track_editor_C_center',
-                  rid='ncap_curve3', interpolate=False)
-    curve3.nodes = _generate_curve(500, 11.5, 270, 362, 2, 11.5, 7)
+    curve3 = Road('track_editor_C_center', rid='ncap_curve3', interpolate=False)
+    curve3.add_nodes(*_generate_curve(500, 11.5, 270, 362, 2, 11.5, 7))
     scenario.add_road(curve3)
 
-    curve4 = Road('track_editor_C_center',
-                  rid='ncap_curve4', interpolate=False)
-    curve4.nodes = _generate_curve(500, 11.5, 90, -2, -2, 11.5, 7)
+    curve4 = Road('track_editor_C_center', rid='ncap_curve4', interpolate=False)
+    curve4.add_nodes(*_generate_curve(500, 11.5, 90, -2, -2, 11.5, 7))
     scenario.add_road(curve4)
 
     road4 = Road('track_editor_C_center', rid='ncap_road4', interpolate=False)
-    road4.nodes = [(-500, 1000, 0, 7), (-500, -1000, 0, 7)]
+    road4.add_nodes((-500, 1000, 0, 7), (-500, -1000, 0, 7))
     scenario.add_road(road4)
 
-    ccrs_waypoint = ScenarioObject('ccr_waypoint', None, 'BeamNGWaypoint',
-                                   (0, -5000, 0.23), (0, 0, 0), (1, 1, 1))
+    ccrs_waypoint = ScenarioObject('ccr_waypoint', None, 'BeamNGWaypoint', (0, -5000, 0.23), (1, 1, 1), (0, 0, 0, 1))
     scenario.add_object(ccrs_waypoint)
 
-    ccftab_waypoint = ScenarioObject('ccftab_waypoint_gvt', None,
-                                     'BeamNGWaypoint', (498.25, -1000, 0.23),
-                                     (0, 0, 0), (1, 1, 1))
+    ccftab_waypoint = ScenarioObject('ccftab_waypoint_gvt', None, 'BeamNGWaypoint',
+                                     (498.25, -1000, 0.23), (1, 1, 1), (0, 0, 0, 1))
     scenario.add_object(ccftab_waypoint)
 
-    ccftab_waypoint2 = ScenarioObject('ccftab_waypoint_vut', None,
-                                      'BeamNGWaypoint', (500, 1000, 0.23),
-                                      (0, 0, 0), (1, 1, 1))  # 501.75
+    ccftab_waypoint2 = ScenarioObject('ccftab_waypoint_vut', None, 'BeamNGWaypoint',
+                                      (500, 1000, 0.23), (1, 1, 1), (0, 0, 0, 1))  # 501.75
     scenario.add_object(ccftab_waypoint2)
 
     vut = Vehicle('vut', model=vut_model, licence='VUT')
     vut.attach_sensor('electrics', Electrics())
     vut.attach_sensor('damage', Damage())
     vut.attach_sensor('timer', Timer())
-    scenario.add_vehicle(vut, (500, 500, 0.23), (0, 0, 0))
+    scenario.add_vehicle(vut, (500, 500, 0.23))
 
     gvt = Vehicle('gvt', model=gvt_model, licence='GVT')
     gvt.attach_sensor('electrics', Electrics())
     gvt.attach_sensor('damage', Damage())
     gvt.attach_sensor('timer', Timer())
-    scenario.add_vehicle(gvt, (505, 500, 0.23), (0, 0, 0))
+    scenario.add_vehicle(gvt, (505, 500, 0.23))
 
     # TODO Add Pedestrian and Cyclist
 
     return scenario
 
 
-def _generate_curve(x, y, angle_start, angle_stop, angle_step, radius, width):
+def _generate_curve(x: float, y: float, angle_start: float, angle_stop: float, angle_step: float, radius: float,
+                    width: float) -> List[Tuple[float, float, float, float]]:
     x_off = x - radius * np.sin(np.radians(angle_start))
     y_off = y - radius * -np.cos(np.radians(angle_start))
 
@@ -115,21 +109,18 @@ def _generate_curve(x, y, angle_start, angle_stop, angle_step, radius, width):
 class NCAPScenario(ABC):
     """
     Abstract base class for all NCAP scenarios.
+    Args:
+        bng (beamngpy.BeamNGpy): BeamNGpy instance.
+        vut_speed (float): Speed of the VUT in km/h.
+        vut_position (tuple): Spawn position of the VUT.
+        vut_rotation (tuple): Spawn rotation quaternion of the VUT.
+        vut_waypoint (str): Name of the waypoint.
     """
 
-    def __init__(self, bng,
-                 vut_speed, vut_position, vut_rotation, vut_waypoint):
-        """
-
-        Args:
-            bng (beamngpy.BeamNGpy): BeamNGpy instance.
-            vut_speed (float): Speed of the VUT in km/h.
-            vut_position (tuple): Spawn position of the VUT.
-            vut_waypoint (str): Name of the waypoint.
-        """
+    def __init__(self, bng: BeamNGpy, vut_speed: float, vut_position: Pos, vut_rotation: Quat, vut_waypoint: str):
         self.bng = bng
-        self.vut = bng.scenario.get_vehicle('vut')
-        self.vut.set_shift_mode('realistic_automatic')
+        self.vut: Vehicle = bng.scenario.get_vehicle('vut')
+        # self.vut.set_shift_mode('realistic_automatic')
         self._vut_speed = vut_speed / 3.6
         self._vut_position = vut_position
         self._vut_rotation = vut_rotation
@@ -153,7 +144,6 @@ class NCAPScenario(ABC):
     def step(self, steps):
         """
         Advances the scenario the given amount of steps.
-
         Args:
             steps (int): The amount of steps to simulate.
         """
@@ -163,7 +153,6 @@ class NCAPScenario(ABC):
     def get_state(self, sensors):
         """
         Returns the state of the test.
-
         Args:
             sensors (dict): The sensor data from both vehicles as a dictionary
                             of dictionaries.
@@ -174,21 +163,19 @@ class NCAPScenario(ABC):
 class CCScenario(NCAPScenario):
     """
     Base class for the car to car scenarios.
+    Args:
+        bng (beamngpy.BeamNGpy): BeamNGpy instance.
+        vut_speed (float): Speed of the VUT in km/h.
+        vut_position (tuple): Spawn position of the VUT.
+        vut_waypoint (str): Name of the waypoint.
+        gvt_speed (float): Speed of the GVT in km/h.
+        gvt_position (tuple): Spawn position of the VUT.
+        gvt_waypoint (str): Name of the waypoint.
     """
 
-    def __init__(self, bng, vut_speed, vut_position, vut_rotation,
-                 vut_waypoint, gvt_speed, gvt_position, gvt_rotation,
-                 gvt_waypoint):
-        """
-        Args:
-            bng (beamngpy.BeamNGpy): BeamNGpy instance.
-            vut_speed (float): Speed of the VUT in km/h.
-            vut_position (tuple): Spawn position of the VUT.
-            vut_waypoint (str): Name of the waypoint.
-            gvt_speed (float): Speed of the GVT in km/h.
-            gvt_position (tuple): Spawn position of the VUT.
-            gvt_waypoint (str): Name of the waypoint.
-        """
+    def __init__(self, bng: BeamNGpy, vut_speed: float, vut_position: Pos, vut_rotation: Quat,
+                 vut_waypoint: str, gvt_speed: float, gvt_position: Pos, gvt_rotation: Quat,
+                 gvt_waypoint: str):
         super().__init__(bng, vut_speed, vut_position, vut_rotation,
                          vut_waypoint)
 
@@ -196,31 +183,28 @@ class CCScenario(NCAPScenario):
         # + 3.6 ) / is a workaround for the ai to reach the given speed
         # TODO: investigate how to get the AI to do that without this hack
 
-        self.gvt = bng.scenario.get_vehicle('gvt')
-        self.gvt.set_shift_mode('realistic_automatic')
+        self.gvt: Vehicle = bng.scenario.get_vehicle('gvt')
+        # self.gvt.set_shift_mode('realistic_automatic')
         self._gvt_speed = gvt_speed / 3.6
         self._gvt_speed_ai = (gvt_speed + 3.6) / 3.6
         self._gvt_position = gvt_position
         self._gvt_rotation = gvt_rotation
         self._gvt_waypoint = gvt_waypoint
 
-    def _teleport_vehicle(self, vehicle, position, rotation=None):
+    def _teleport_vehicle(self, vehicle: Vehicle, position: Pos, rotation: Quat | None = None):
         """
         Teleports the given vehicle to the given position with the given
         rotation.
-
         Args:
             vehicle (beamngpy.Vehicle): The vehicle to teleport.
             position (tuple) The target position as an (x, y, z) tuple
                              containing world-space coordinates.
-            rotation (tuple): Optional tuple specifying rotations around the
-                              (x, y, z) axes in degrees.
-
+            rotation (tuple): Optional tuple specifying the rotation as a quaternion.
         Notes:
             This method is a workaround that ignores the refnode of the vehicle
             and uses the center of the boundingbox instead.
         """
-        self.bng.teleport_vehicle(vehicle, position, rotation)
+        vehicle.teleport(position, rotation)
         position = np.array(position)
 
         center = np.zeros(3)
@@ -230,27 +214,24 @@ class CCScenario(NCAPScenario):
         offset = position - center / 8
         offset[2] = 0  # Ignore z coordinate
 
-        self.bng.teleport_vehicle(vehicle, list(position + offset), rotation)
+        vehicle.teleport(list(position + offset), rotation)
 
-    def _observe(self):
+    def _observe(self) -> Dict[str, Dict]:
         """
         Retrieves sensor values for the sensors attached to the VUT and GVT and
         returns them as a dict.
-
         Returns:
             The sensor data from both vehicles as a dictionary of dictionaries.
-
         Notes:
             Example: observation['vut']['electrics']['wheelspeed']
         """
         observation = dict()
         for vehicle in [self.vut, self.gvt]:
-            vehicle.update_vehicle()
+            vehicle.poll_sensors()
             sensors = dict()
-            sensors['state'] = vehicle.state
 
-            for key, value in self.bng.poll_sensors(vehicle).items():
-                sensors[key] = value
+            for key, value in vehicle.sensors.items():
+                sensors[key] = value.data
 
             observation[vehicle.vid] = sensors
 
@@ -268,10 +249,9 @@ class CCScenario(NCAPScenario):
             vut_speed = observation['vut']['electrics']['wheelspeed']
             gvt_speed = observation['gvt']['electrics']['wheelspeed']
 
-    def _cars_reached_speed(self, vut_speed, gvt_speed):  # TODO
+    def _cars_reached_speed(self, vut_speed: float, gvt_speed: float):  # TODO
         """
         Checks whether both cars have reached the desired speed.
-
         Notes:
             See [1], page 20, section 8.4.2
         """
@@ -281,19 +261,16 @@ class CCScenario(NCAPScenario):
 
         return vut and gvt
 
-    def get_state(self, sensors):
+    def get_state(self, sensors: Dict[str, Dict]) -> int:
         """
         Returns the state of the test.
-
         Args:
             sensors (dict): The sensor data from both vehicles as a dictionary
             of dictionaries.
-
         Returns:
             0 = Non terminal state
             1 = Success (V_VUT = 0km/h or V_VUT < V_GVT)
            -1 = Failure (Contact between VUT and GVT)
-
         Notes:
             See [1], page 21, section 8.4.3
         """
@@ -316,21 +293,21 @@ class CCRScenario(CCScenario):
     Base class for the car to car rear scenarios.
     """
 
-    def __init__(self, bng, vut_speed, gvt_speed, distance, overlap=1.0):
+    def __init__(self, bng: BeamNGpy, vut_speed: float, gvt_speed: float, distance: float, overlap: float = 1.0):
         """
         Args:
             bng (:class:`.BeamNGpy`): BeamNGpy instance.
             vut_speed (float): Speed of the VUT in km/h.
             gvt_speed (float): Speed of the GVT in km/h.
-            distance (int): Distance between the Cars in m.
+            distance (float): Distance between the Cars in m.
             overlap (float): Percentage of the width of the VUT overlapping
                              the GVT.
         """
-        super().__init__(bng, vut_speed, None, (0, 0, 0), 'ccr_waypoint',
-                         gvt_speed, (0, -1000, 0.21), (0, 0, 0),
+        super().__init__(bng, vut_speed, (0, 0, 0), (0, 0, 0, 1), 'ccr_waypoint',
+                         gvt_speed, (0, -1000, 0.21), (0, 0, 0, 1),
                          'ccr_waypoint')
         self._overlap = overlap / 100
-        self._initial_distance = 1000
+        self._initial_distance = 100
         self._distance = distance
 
     def load(self):
@@ -372,7 +349,7 @@ class CCRScenario(CCScenario):
             position = vut_cf + np.abs(dist_diff) * dv
             position[2] = self.vut.state['pos'][2]
 
-            self.bng.teleport_vehicle(self.vut, list(position))
+            self.vut.teleport(list(position), reset=False)
 
             # TODO Workaround for ref-nodes
             vut_bbox = self.vut.get_bbox()
@@ -381,7 +358,7 @@ class CCRScenario(CCScenario):
             vut_cf = (vut_cf_a + vut_cf_b) / 2
             offset = position - vut_cf
             offset[2] = 0  # Ignore z coordinate
-            self.bng.teleport_vehicle(self.vut, list(position + offset))
+            self.vut.teleport(list(position + offset), reset=False)
 
         return self._observe()
 
@@ -407,10 +384,8 @@ class CCRScenario(CCScenario):
     def step(self, steps):
         """
         Advances the scenario the given amount of steps.
-
         Args:
             steps (int): The amount of steps to simulate.
-
         Returns:
             A Dictionary with the sensor data from the VUT and GVT.
         """
@@ -451,9 +426,11 @@ class CCRScenario(CCScenario):
         dv /= np.linalg.norm(dv)
 
         position = point_front + self._initial_distance * dv
+
+        self.gvt.poll_sensors()  # sensors have to be polled once so the vehicle state gets populated
         position[2] = self.gvt.state['pos'][2]
 
-        self.bng.teleport_vehicle(self.vut, list(position), self._vut_rotation)
+        self.vut.teleport(list(position), self._vut_rotation)
 
         vut_bbox = self.vut.get_bbox()
         vut_fc_a = np.array(vut_bbox['front_bottom_left'])
@@ -463,8 +440,7 @@ class CCRScenario(CCScenario):
         offset = position - vut_fc
         offset[2] = 0  # Ignore z coordinate
 
-        self.bng.teleport_vehicle(self.vut, list(position + offset),
-                                  self._vut_rotation)
+        self.vut.teleport(list(position + offset), self._vut_rotation)
 
 
 class CCRS(CCRScenario):
@@ -472,10 +448,9 @@ class CCRS(CCRScenario):
     Implementation of the Car-to-Car Rear stationary scenario.
     """
 
-    def __init__(self, bng, vut_speed, overlap):
+    def __init__(self, bng: BeamNGpy, vut_speed: float, overlap: int):
         """
         Instantiates the Scenario.
-
         Args:
             bng (beamng.BeamNGpy): BeamNGpy instance.
             vut_speed (float): Speed of the VUT in km/h.
@@ -494,10 +469,9 @@ class CCRM(CCRScenario):
     Implementation of the Car-to-Car Rear moving scenario.
     """
 
-    def __init__(self, bng, vut_speed, overlap):
+    def __init__(self, bng: BeamNGpy, vut_speed: float, overlap: int):
         """
         Instantiates the Scenario.
-
         Args:
             bng (beamng.BeamNGpy): BeamNGpy instance.
             vut_speed (float): Speed of the VUT in km/h.
@@ -519,7 +493,6 @@ class CCRB(CCRScenario):
     def __init__(self, bng, deceleration, distance, overlap=100):
         """
         Instantiates the Scenario.
-
         Args:
             bng (beamng.BeamNGpy): BeamNGpy instance.
             deceleration (float): Deceleration of the GVT in m/s^2.
@@ -544,10 +517,8 @@ class CCRB(CCRScenario):
     def step(self, steps):
         """
         Advances the scenario the given amount of steps.
-
         Args:
             steps (int): The amount of steps to simulate.
-
         Returns:
             A Dictionary with the sensor data from the VUT and GVT.
         """
